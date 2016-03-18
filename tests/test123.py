c215f588fb5c7c36ac96ae5530ce5b3f62d4cd47
avu2411 <vrmavantika@gmail.com>
2016-03-18 18:45:38 +0530

    test123 changed pieces values changed

--- tests/test123.py
+++ tests/test123.py
@@ -1,7 +1,24 @@
 import unittest
 import bencode
-abc = bencode.bdecode(open('hello.txt.torrent', 'rb').read())
+abc = bencode.bdecode(open('try.torrent', 'rb').read())
 str2 = "announce";
 print abc
-print abc['info']['length']
+print abc['comment']
+
+
+n=len(abc['info']['files']);
+
+for i in range(n):
+	print abc['info']['files'][i]['path']
+	print abc['info']['files'][i]['length']
+	
+
+print abc['info']['piece length']
+print abc['info']['name']
+
+#print abc['info']['pieces']
+
+
+print abc['creation date']
+print abc['announce-list']
 print abc['announce']
